e3168ae0baf2ace325758006f71d63eb38f6009d
Jakob Scharrer <scharrer.ja@gmail.com>
2020-03-01 16:19:04 +0100

    Basic Version of an interactable book

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    [Header("Animated Pages")]
+    [SerializeField] private Page page1 = null;
+    [SerializeField] private Page page2 = null;
+    [SerializeField] private Page page3 = null;
+
+    [Header("Page Content")]
+    [SerializeField] private PageContent[] pages;
+    
+    private int _pageIndex;
+    private bool _animating;
+    
+    private Page LeftPage => GetPageByIndex(_pageIndex);
+    private Page RightPage => GetPageByIndex(_pageIndex + 2);
+    private Page StandbyPage => GetPageByIndex(_pageIndex + 4);
+
+    private void Start()
+    {
+        StandbyPage.Disable();
+        LeftPage.EnableLeft(null, GetTextureByIndex(0));
+        RightPage.EnableRight(GetTextureByIndex(1), GetTextureByIndex(2));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TryForward();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            TryBackward();
+        }
+    }
+
+    private void TryForward()
+    {
+        if (_animating)
+            return;
+
+        if (_pageIndex == pages.Length - 2)
+            return;
+        
+        StartCoroutine(Forward());
+    }
+
+    private void TryBackward()
+    {
+        if (_animating)
+            return;
+
+        if (_pageIndex == 0)
+            return;
+        
+        StartCoroutine(Backward());
+    }
+
+    private IEnumerator Forward()
+    {
+        var newPageIndex = _pageIndex + 2;
+        
+        _animating = true;
+        RightPage.GoFromRightToLeft();
+        StandbyPage.EnableRight(GetTextureByIndex(newPageIndex + 1), GetTextureByIndex(newPageIndex + 2));
+        
+        yield return new WaitForSeconds(RightPage.GoRightAnimationLength);
+        
+        _animating = false;
+        LeftPage.Disable();
+        
+        _pageIndex = newPageIndex;
+    }
+    
+    private IEnumerator Backward()
+    {
+        var newPageIndex = _pageIndex - 2;
+        
+        _animating = true;
+        LeftPage.GoFromLeftToRight();
+        StandbyPage.EnableLeft(GetTextureByIndex(newPageIndex - 1), GetTextureByIndex(newPageIndex));
+        
+        yield return new WaitForSeconds(LeftPage.GoLeftAnimationLength);
+        
+        _animating = false;
+        RightPage.Disable();
+        
+        _pageIndex = newPageIndex;
+    }
+
+    private Texture2D GetTextureByIndex(int index)
+    {
+        if (index < 0 || index >= pages.Length)
+        {
+            return null;
+        }
+        
+        return pages[index].BackgroundTexture;
+    }
+    
+    /*
+     * In:  -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | ...
+     * Out:  3 |  1 | 1 | 2 | 2 | 3 | 3 | 1 | 1 | 2 | ...
+     */
+    private Page GetPageByIndex(int index)
+    {
+        index = Mathf.CeilToInt(index / 2f) + 1;
+        index = (index % 3 + 3) % 3;
+        
+        switch (index)
+        {
+            case 1: return page1;
+            case 2: return page2;
+            case 0: return page3;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/Page.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public  class Page : MonoBehaviour
+{
+    [SerializeField] private Renderer leftRenderer;
+    [SerializeField] private Renderer rightRenderer;
+    
+    private Animator _animator;
+    
+    private static readonly int goRight = Animator.StringToHash("goRight");
+    private static readonly int goLeft = Animator.StringToHash("goLeft");
+    private static readonly int right = Animator.StringToHash("right");
+    private static readonly int left = Animator.StringToHash("left");
+
+    private AnimationClip _goRightAnimation;
+    private AnimationClip _goLeftAnimation;
+
+    public float GoRightAnimationLength => _goRightAnimation.length;
+    public float GoLeftAnimationLength => _goLeftAnimation.length;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        _goRightAnimation = _animator.runtimeAnimatorController.animationClips.FirstOrDefault(clip => clip.name.Equals("GoRight"));
+        _goLeftAnimation = _animator.runtimeAnimatorController.animationClips.FirstOrDefault(clip => clip.name.Equals("GoLeft"));
+    }
+
+    public void GoFromRightToLeft()
+    {
+        _animator.SetTrigger(right);
+        _animator.SetTrigger(goRight);
+    }
+
+    public void GoFromLeftToRight()
+    {
+        _animator.SetTrigger(left);
+        _animator.SetTrigger(goLeft);
+    }
+
+    public void EnableLeft(Texture2D background1, Texture2D background2)
+    {
+        gameObject.SetActive(true);
+        _animator.SetTrigger(left);
+        
+        SetRightTexture(background1);
+        SetLeftTexture(background2);
+    }
+    
+    public void EnableRight(Texture2D background1, Texture2D background2)
+    {
+        gameObject.SetActive(true);
+        _animator.SetTrigger(right);
+        
+        SetRightTexture(background1);
+        SetLeftTexture(background2);
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void SetLeftTexture(Texture2D texture)
+    {
+        if (texture == null)
+        {
+            return;
+        }
+        
+        leftRenderer.material.mainTexture = texture;
+    }
+
+    private void SetRightTexture(Texture2D texture)
+    {
+        if (texture == null)
+        {
+            return;
+        }
+        
+        rightRenderer.material.mainTexture = texture;
+    }
+}
