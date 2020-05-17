77464be3e833bbcce1bc23b3500232ab872f1fcf
Leonid <leonidb93@gmail.com>
2020-05-17 19:14:14 +0200

    implement basic vr drawing on a plane

---
+++ Assets/Scripts/VR/Line.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Line {
+    // Fields for JSON file
+    public Vector3 colour;
+    public Vector3[] Positions;
+
+
+
+    private Color Colour;
+    private Vector3Storer[] SerializablePositions;
+    private Vector2[] PositionsForDatabase;
+
+
+
+    public Line(Color drawnColour, Vector3[] positions)
+    {
+        
+        Colour = drawnColour;
+        Positions = positions;
+        PositionsForDatabase = MyVector3Extension.toVector2(positions);
+        SerializePositions();
+        colour.x = Colour.r;
+        colour.y = Colour.g;
+        colour.z = Colour.b;
+    }
+
+    public void SerializePositions()
+    {
+        SerializablePositions = new Vector3Storer[Positions.Length];
+        for (int i = 0; i < Positions.Length; i++)
+        {
+            float x = Positions[i].x;
+            float y = Positions[i].y;
+            float z = Positions[i].z;
+            SerializablePositions[i] = new Vector3Storer(x, y, z);
+        }
+    }
+
+}
+
+// Source: https://answers.unity.com/questions/1034471/c-convert-vector3-to-vector2.html
+public static class MyVector3Extension
+{
+    public static Vector2[] toVector2(this Vector3[] v3)
+    {
+        return System.Array.ConvertAll<Vector3, Vector2>(v3, getV3fromV2);
+    }
+
+    public static Vector2 getV3fromV2(Vector3 v3)
+    {
+        return new Vector2(v3.x, v3.y);
+    }
+}
+
+[System.Serializable]
+public class Vector3Storer
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector3Storer(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
---
+++ Assets/Scripts/VR/SwipeTrail.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class SwipeTrail : MonoBehaviour
+{
+    public LineManager lineManager = new LineManager();
+
+    public int LineCounter = -1;
+
+    public GameObject PlaneToDrawOn;
+
+    public float lineWidth = 0.75f;
+
+    public Material TrailMaterial;
+    public bool TouchedAlready = false;
+    private TrailRenderer TrailRenderer;
+
+
+    public bool FirstTouch = true;
+
+
+    public Color Color;
+
+
+    public static String LastMarkerName;
+
+    private Vector3 StandardPosition;
+
+    void Awake()
+    {
+
+        StandardPosition = transform.position;
+
+        TrailRenderer = GetComponentInChildren<TrailRenderer>();
+        TrailRenderer.startWidth = lineWidth;
+        TrailRenderer.endWidth = lineWidth;
+
+        // Disabled to get rid of the accidental line in the first frame
+        TrailRenderer.enabled = false;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogError("ENTERED");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.transform.gameObject.name == PlaneToDrawOn.name)
+        {
+            TrailRenderer.enabled = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.transform.gameObject.name == PlaneToDrawOn.name)
+        {
+            TrailRenderer.enabled = false;
+            StoreRay();
+            CreateLineObject();
+            print("exit");
+        }
+
+    }
+
+
+    void Update()
+    {
+
+        //bool fingerOnScreen = Input.GetMouseButton(0);
+
+        //if (fingerOnScreen)
+        //{
+        //    if (!TouchedAlready)
+        //    {
+        //        TouchedAlready = true;
+        //    }
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out hit) && !EventSystem.current.IsPointerOverGameObject())
+        //    {
+        //        transform.position = hit.point;
+        //    }
+        //}
+
+        //// To avoid an additional line
+        //if (fingerOnScreen && FirstTouch)
+        //{
+        //    FirstTouch = false;
+        //    setTrailColour(Color, TrailRenderer);
+        //}
+
+        //if (!fingerOnScreen)
+        //{
+        //    TrailRenderer.enabled = false;
+        //}
+
+        //// Check, if the user has released the screen to store the last drawn line
+        //if (!fingerOnScreen && TouchedAlready)
+        //{
+        //    StoreRay();
+        //    CreateLineObject();
+        //    TouchedAlready = false;
+        //}
+
+
+
+    }
+
+
+
+    //! The created object is used for retracing the line
+    private void CreateLineObject()
+    {
+        LineCounter++;
+        GameObject newLine = new GameObject();
+        newLine.transform.parent = PlaneToDrawOn.transform;
+        newLine.name = "Line segment " + LineCounter;
+        newLine.AddComponent<LineRenderer>();
+
+        RetraceLine(newLine.GetComponent<LineRenderer>(), LineCounter);
+
+    }
+
+
+    private void RetraceLine(LineRenderer line, int lineNumber)
+    {
+        Line currentLine = lineManager.AllLines[lineNumber];
+        // Set the width of the Line Renderer
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        // Set the number of vertex for the Line Renderer
+        line.positionCount = currentLine.Positions.Length;
+        line.material = TrailMaterial;
+        setTrailColour(Color, null, line);
+        line.SetPositions(currentLine.Positions);
+        line.useWorldSpace = false;
+    }
+
+
+
+    private void StoreRay()
+    {
+        print("store ray");
+        int arrayLength = TrailRenderer.positionCount;
+        Color currentColour = TrailRenderer.endColor;
+        Vector3[] rayPositions = new Vector3[arrayLength];
+        GetComponentInChildren<TrailRenderer>().GetPositions(rayPositions);
+
+        lineManager.AllLines.Add(new Line(currentColour, rayPositions));
+
+
+        // Clears the "stage" so you can draw a new line
+        TrailRenderer.Clear();
+        FirstTouch = true;
+        TrailRenderer.enabled = false;
+    }
+
+
+
+
+
+    /// <summary>
+    /// Changes the Colour of the passed Renderer component
+    /// </summary>
+    /// <param name="color"></param>
+    /// <param name="tRenderer">Optional. Changes the colour of the passed trail renderer</param>
+    /// <param name="lRenderer">Optional. Changes the colour of the passed line renderer</param>
+    public void setTrailColour(Color color, TrailRenderer tRenderer = null, LineRenderer lRenderer = null)
+    {
+        if (tRenderer != null)
+        {
+            tRenderer.startColor = color;
+            tRenderer.endColor = color;
+        }
+        else if (lRenderer != null)
+        {
+            lRenderer.startColor = color;
+            lRenderer.endColor = color;
+        }
+    }
+
+    public void SelectColor(String language)
+    {
+
+
+        setTrailColour(Color, TrailRenderer);
+
+
+        TrailRenderer.enabled = false;
+        transform.position = StandardPosition;
+
+    }
+
+}
+
+[System.Serializable]
+public class LineManager
+{
+    public List<Line> AllLines = new List<Line>();
+}
