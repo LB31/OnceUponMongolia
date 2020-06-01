f9fc2de8f1569ce7d9ab6f3eef48f34e8b2fe697
Leonid <leonidb93@gmail.com>
2020-06-01 04:18:42 +0200

    implement custom haptics for controllers

--- Assets/Scripts/VR/BookMenuController.cs
+++ Assets/Scripts/VR/BookMenuController.cs
@@ -27,7 +27,7 @@ public class BookMenuController : MonoBehaviour
 
     private void TurnPageRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Pulse(1, 150, 75, fromSource);
+        StartCoroutine(Pulse(1, 1, 5, 1, fromSource));
     }
 
     private void TurnPageLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
@@ -45,9 +45,14 @@ public class BookMenuController : MonoBehaviour
         throw new NotImplementedException();
     }
 
-    private void Pulse(float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
+    private IEnumerator Pulse(int repeats, float duration, float frequency, float amplitude, SteamVR_Input_Sources source)
     {
-        Vibration.Execute(0, duration, frequency, amplitude, source);
+        for (int i = 0; i < repeats; i++)
+        {
+            Vibration.Execute(0, duration, frequency, amplitude, source);
+            yield return new WaitForSeconds(0.1f);
+        }
+
     }
 
 }
