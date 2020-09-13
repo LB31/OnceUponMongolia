0239112fec61288756550cd4f1fd2055112c47be
Leonid <leonidb93@gmail.com>
2020-09-13 14:44:00 +0200

    implement transition effect for teleporting

--- Assets/_Scripts/PositionChangeTrigger.cs
+++ Assets/_Scripts/PositionChangeTrigger.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class PositionChangeTrigger : MonoBehaviour
 {
     public Transform NextCam;
     public Transform PreviousCam;
+    public float FadeDuration = 2;
 
-    public Transform Player;
-
+    private Transform Player;
+    private Image SceneChanger;
     private bool nextCam;
+    private Vector3 direction;
 
     private void Start()
     {
         Player = FindObjectOfType<XRRig>().transform;
+        SceneChanger = GameObject.Find("WorldSwitchCanvas")
+            .transform.GetChild(0).GetComponent<Image>();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Vector3 direction = other.transform.position - transform.position;
+        direction = other.transform.position - transform.position;
+        StartCoroutine(VisualizeSceneChange());
+    }
+
+    public IEnumerator VisualizeSceneChange()
+    {
+        SceneChanger.enabled = true;
+        Color color = SceneChanger.color;
+        for (float i = 0; i <= 1; i += Time.deltaTime * FadeDuration)
+        {
+            color.a = i;
+            SceneChanger.color = color;
+            yield return null;
+        }
+
+        ChangePosition();
+
+        for (float i = 1; i >= 0; i -= Time.deltaTime * FadeDuration)
+        {
+            color.a = i;
+            SceneChanger.color = color;
+            yield return null;
+        }
+
+        SceneChanger.enabled = false;
+    }
+
+    private void ChangePosition()
+    {
         if (Vector3.Dot(transform.forward, direction) > 0)
         {
             print("Front");
@@ -27,8 +60,9 @@ public class PositionChangeTrigger : MonoBehaviour
             {
                 nextCam = true;
                 Player.position = NextCam.position;
+                Player.rotation = NextCam.rotation;
             }
-            
+
         }
         if (Vector3.Dot(transform.forward, direction) < 0)
         {
@@ -37,8 +71,10 @@ public class PositionChangeTrigger : MonoBehaviour
             {
                 nextCam = false;
                 Player.position = PreviousCam.position;
+                Player.rotation = PreviousCam.rotation;
             }
-                
+
         }
     }
+
 }
