065f6c90bf9a27d015319ddd30fc9fe77511d810
Leonid <leonidb93@gmail.com>
2020-09-13 04:10:48 +0200

    implement girl movement

--- Assets/_Scripts/VR/GirlController.cs
+++ Assets/_Scripts/VR/GirlController.cs
@@ -1,45 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class ContinuousMovement : MonoBehaviour
+public class GirlController : MonoBehaviour
 {
+
     public float Speed = 1;
     public XRNode InputSource;
     public float AdditionalHeight = 0.2f;
 
-    private XRRig rig; 
+    private XRRig rig;
     private Vector2 inputAxis;
     private CharacterController character;
     private InputDevice device;
     private float gravity = -9.81f;
     private float fallingSpeed;
 
+    // Testing
+    public Transform Walkie;
+
+
     void Start()
     {
         rig = GetComponent<XRRig>();
-        character = GetComponent<CharacterController>();
+        character = Walkie.GetComponent<CharacterController>();
         device = InputDevices.GetDeviceAtXRNode(InputSource);
     }
 
     void Update()
-    {  
+    {
         device.TryGetFeatureValue(CommonUsages.secondary2DAxis, out inputAxis);
+        print(inputAxis);
     }
 
     private void FixedUpdate()
     {
-        CapsuleFollowHeadset();
+        if (inputAxis.magnitude > 0.6f)
+        {
+            //Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            //characterController.Move(WalkingSpeed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
 
-        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+            Walkie.Rotate(Vector3.up * inputAxis.x * Speed);
+        }
 
-        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+        float spinOffset = 0.8f;
 
-        if (inputAxis.magnitude > 0.15f)
-            character.Move(direction * Time.fixedDeltaTime * Speed);
+        if (inputAxis.y > spinOffset || inputAxis.y < -spinOffset)
+        {
+            character.Move(Walkie.forward * inputAxis.y / Speed);
+        }
 
         // gravity 
         if (character.isGrounded)
@@ -50,11 +62,5 @@ public class ContinuousMovement : MonoBehaviour
         character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
     }
 
-    private void CapsuleFollowHeadset()
-    {
-        character.height = rig.cameraInRigSpaceHeight + AdditionalHeight;
-        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
-        character.center = new Vector3(capsuleCenter.x, character.height * 0.5f + character.skinWidth, capsuleCenter.z);
-    }
 
 }
