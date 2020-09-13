b6729600ac9c720e93563ad70278eb9d48f84974
Leonid <leonidb93@gmail.com>
2020-09-13 19:40:56 +0200

    reafactor spinning and girl movement to both headsets
    Former-commit-id: eaa5283d9a40f7caabb55557e61904fd14cfc887

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public bool OculusInUse;
+    public SnapTurnProvider stp;
 
     private void Awake() {
         if (Instance)
             return;
         Instance = this;
+    }
+
+    private void Start()
+    {
+        SnapTurnProvider stp = FindObjectOfType<SnapTurnProvider>();
+        if (OculusInUse)
+            stp.turnUsage = SnapTurnProvider.InputAxes.Primary2DAxis;
+        else
+            stp.turnUsage = SnapTurnProvider.InputAxes.Secondary2DAxis;
     }
 }
