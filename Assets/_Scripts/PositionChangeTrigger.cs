3e4a8d5cb1bb92cea32b8fbc357e9f7966c530af
Leonid <leonidb93@gmail.com>
2020-09-13 04:35:01 +0200

    implement movement trigger for camera switching
    Former-commit-id: dcceca78ece5ca2760c338f299e1fafcaa740c42

---
+++ Assets/_Scripts/PositionChangeTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PositionChangeTrigger : MonoBehaviour
+{
+    public Transform NextCam;
+    public Transform PreviousCam;
+
+    public Transform Player;
+
+    private bool nextCam;
+
+    private void Start()
+    {
+        Player = FindObjectOfType<XRRig>().transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Vector3 direction = other.transform.position - transform.position;
+        if (Vector3.Dot(transform.forward, direction) > 0)
+        {
+            print("Front");
+            if (!nextCam)
+            {
+                nextCam = true;
+                Player.position = NextCam.position;
+            }
+            
+        }
+        if (Vector3.Dot(transform.forward, direction) < 0)
+        {
+            print("Back");
+            if (nextCam)
+            {
+                nextCam = false;
+                Player.position = PreviousCam.position;
+            }
+                
+        }
+    }
+}
