987dd49cdb3dd3f59e2204c85af754bda050b8e5
Leonid <leonidb93@gmail.com>
2020-11-03 03:56:15 +0100

    implement basic gambling level
    Former-commit-id: edc3750cefe2b66615cf83da0c199ff624169a9f

---
+++ Assets/_Scripts/GamblingManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GamblingManager : MonoBehaviour
+{
+    public List<string> OracleAnswers;
+    public TextMeshProUGUI OracleText;
+    private int stonesInside;
+
+    private void ShowOracleMessage()
+    {
+        int answer = Random.Range(0, OracleAnswers.Count);
+        OracleText.text = OracleAnswers[answer];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Dice"))
+        {
+            stonesInside++;
+        }
+
+        if (stonesInside == 4)
+        {
+            ShowOracleMessage();
+            Debug.LogError("all inside");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Dice"))
+        {
+            stonesInside--;
+        }
+    }
+
+}
