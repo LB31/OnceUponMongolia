1b8ec9a027564e3150bf9dbe8cf7660ec0661c88
Leonid <leonidb93@gmail.com>
2021-01-09 08:33:26 +0100

    start implement PlayMaker structure
    Former-commit-id: 00a0c5bce0c3eddb17d38861392e09a71d6ed5db

---
+++ Assets/_Scripts/VR/RayControllerVR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RayControllerVR : MonoBehaviour
+{
+    private XRController xrController;
+    private XRRayInteractor rayInteractor;
+
+    private void Awake()
+    {
+        xrController = GetComponent<XRController>();
+        rayInteractor = GetComponent<XRRayInteractor>();
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        xrController.moveObjectOut = InputHelpers.Button.SecondaryAxis2DUp;
+
+        rayInteractor.maxRaycastDistance = 10;
+        rayInteractor.lineType = XRRayInteractor.LineType.ProjectileCurve;
+    }
+}
