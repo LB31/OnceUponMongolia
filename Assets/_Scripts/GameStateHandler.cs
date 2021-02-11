ce4bf9c54355992edffd68716655afc906d97425
kirrikirri <lucia.x.zhai@my.mail.de>
2021-02-11 20:29:31 +0100

    start implementing state manager for vr player, warp player to object of choice, experiment with post processing

---
+++ Assets/_Scripts/GameStateHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GameStateHandler : MonoBehaviour
+{
+    public CharacterController characterController;
+    public ContinuousMovement continuousMovement;
+    public DeviceBasedSnapTurnProvider snapTurnProvider;
+    public PositionChanger positionChanger;
+    public GirlController girlController;
+
+    private List<MonoBehaviour> allComponents = new List<MonoBehaviour>();
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        continuousMovement = GetComponent<ContinuousMovement>();
+        snapTurnProvider = GetComponent<DeviceBasedSnapTurnProvider>();
+        positionChanger = GetComponent<PositionChanger>();
+        girlController = GetComponent<GirlController>();
+
+        //allComponents.Add(characterController);
+        allComponents.Add(continuousMovement);
+        allComponents.Add(snapTurnProvider);
+        allComponents.Add(positionChanger);
+        allComponents.Add(girlController);
+
+        DisableAllComponents();
+
+        // test
+        MoveFreely();
+    }
+
+    public void DisableAllComponents(bool snapTurn = true)
+    {
+        foreach (MonoBehaviour component in allComponents)
+        {
+            component.enabled = false;
+        }
+        characterController.enabled = false;
+        snapTurnProvider.enabled = snapTurn;
+    }
+
+    public void MoveFreely()
+    {
+        DisableAllComponents();
+
+        characterController.enabled = true;
+        continuousMovement.enabled = true;
+    }
+
+    public void ControlGirl()
+    {
+        DisableAllComponents();
+
+        positionChanger.enabled = true;
+        girlController.enabled = true;
+    }
+
+
+
+}
