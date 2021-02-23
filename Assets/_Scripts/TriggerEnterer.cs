7c06d554b5a19891cea5cad14839557fe60f86a5
Leonid Barsht <leonidb93@gmail.com>
2021-02-23 04:45:48 +0100

    bring vero, boat and hailstone to other river side and back

--- Assets/_Scripts/TriggerEnterer.cs
+++ Assets/_Scripts/TriggerEnterer.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.AI;
 
 /// <summary>
 /// For Vero entering buildings and stuff
@@ -10,6 +14,19 @@ using UnityEngine.UI;
 public class TriggerEnterer : MonoBehaviour
 {
     public float FadeSpeed = 0.5f;
+
+    public List<CustomTransform> VeroPositions;
+    // Teleport Positions
+    public List<Vector3> BoatPositions = new List<Vector3>()
+    {
+        new Vector3(70f, 2.8f, 97f), // boat right origin
+        new Vector3(87f, 2.8f, 93f), // boat right temple
+        new Vector3(72f, 2.8f, 103f), // boat left origin
+        new Vector3(87f, 2.8f, 100f), // boat left temple
+    };
+
+    private bool onTempleSide;
+    private bool inTrigger;
 
     private Transform veroCams;
     private List<Vector3> originCamPositions = new List<Vector3>();
@@ -34,21 +51,52 @@ public class TriggerEnterer : MonoBehaviour
             originCamPositions.Add(cam.localPosition);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private async void OnTriggerEnter(Collider other)
     {
+        if (inTrigger) return;
+        inTrigger = true;
+
         if (other.name.Contains("YurteCollider") && other is CapsuleCollider)
             ChangeCams(true);
         if (other.name.Contains("boat"))
         {
             // TODO boat action
-            // Fade screen
+
+            // New Vero Position
+            Location nextLocation = onTempleSide ? Location.Harbor : Location.Temple;
+            StartCoroutine(VisualizeSceneChange(GetNextTransform(nextLocation)));
+
+            // New Boat Positon
+            await Task.Delay(1000);
+            Vector3 nextBoatPos;
+            if (other.name.Contains("Right"))
+                nextBoatPos = onTempleSide ? BoatPositions[0] : BoatPositions[1];
+            else
+                nextBoatPos = onTempleSide ? BoatPositions[2] : BoatPositions[3];
+            other.transform.position = nextBoatPos;
+
+            // Teleport HailStone when he is following Vero
+            VillagerController hailStone = GameManager.Instance.HailStone;
+            if (hailStone.FollowVero)
+            {
+                hailStone.GetComponent<NavMeshAgent>().enabled = false;
+                float offset = onTempleSide ? -5 : 5;
+                hailStone.transform.position = GetNextTransform(nextLocation).position + new Vector3(offset, 0, 0);
+                hailStone.GetComponent<NavMeshAgent>().enabled = true;
+            }
+
+
+            onTempleSide = !onTempleSide;
             // move Vero, HailStone and hit boat to other side
         }
         if (other.name.Contains("water"))
         {
             // Respawn
-            StartCoroutine(VisualizeSceneChange(new Vector3(60f, 5f, 103)));
+            Location nextLocation = onTempleSide ? Location.Temple : Location.Harbor;
+            StartCoroutine(VisualizeSceneChange(GetNextTransform(nextLocation)));
         }
+
+        inTrigger = false;
     }
 
     private void OnTriggerExit(Collider other)
@@ -57,6 +105,16 @@ public class TriggerEnterer : MonoBehaviour
             ChangeCams(false);
     }
 
+    private Transform GetNextTransform(Location location)
+    {
+        CustomTransform nextTransform = VeroPositions.Find(l => l.Location == location);
+        Transform nextVeroPos = new GameObject().transform;
+        nextVeroPos.position = nextTransform.Position;
+        nextVeroPos.rotation = Quaternion.Euler(nextTransform.Rotation);
+        return nextVeroPos;
+    }
+
+    // For entering yurts
     private void ChangeCams(bool inHouse)
     {
         for (int i = 0; i < veroCams.childCount; i++)
@@ -68,14 +126,21 @@ public class TriggerEnterer : MonoBehaviour
         }
     }
 
-    private void PortCharacter(Vector3 newPos)
+    private void TeleportCharacter(Transform trans)
     {
         GetComponent<CharacterController>().enabled = false;
-        transform.position = newPos;
+        transform.position = trans.position;
+        transform.rotation = trans.localRotation;
+        GetComponent<CharacterController>().enabled = true;
+    }
+    private void TeleportCharacter(Vector3 pos)
+    {
+        GetComponent<CharacterController>().enabled = false;
+        transform.position = pos;
         GetComponent<CharacterController>().enabled = true;
     }
 
-    public IEnumerator VisualizeSceneChange(Vector3 newPos)
+    public IEnumerator VisualizeSceneChange(Transform newPos, Vector3? pos = null)
     {
         sceneChanger.enabled = true;
         Color color = sceneChanger.color;
@@ -89,8 +154,11 @@ public class TriggerEnterer : MonoBehaviour
         color.a = 1;
         sceneChanger.color = color;
 
-        // TODO something in between
-        PortCharacter(newPos);
+        // Do something in between
+        if (pos == null)
+            TeleportCharacter(newPos);
+        else
+            TeleportCharacter(pos.Value);
 
         yield return new WaitForSeconds(1);
 
@@ -103,4 +171,18 @@ public class TriggerEnterer : MonoBehaviour
 
         sceneChanger.enabled = false;
     }
+}
+
+[Serializable]
+public class CustomTransform
+{
+    public Location Location;
+    public Vector3 Position;
+    public Vector3 Rotation;
+}
+
+public enum Location
+{
+    Temple,
+    Harbor,
 }
