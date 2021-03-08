b43fdcda3351b7ca0258174d239e5c63cacf078e
Leonid Barsht <leonidb93@gmail.com>
2021-03-08 11:11:58 +0100

    port dice level to main scene, add answer dialogs with granny

--- Assets/_Scripts/Gameplay/GamblingManager.cs
+++ Assets/_Scripts/Gameplay/GamblingManager.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
 public class GamblingManager : MonoBehaviour
-{
-    public List<string> OracleAnswers;
-    public TextMeshProUGUI OracleText;
+{ 
+    public Transform DiceParent;
+    private List<string> OracleAnswers;
+    private TextMeshProUGUI OracleText;
     [SerializeField] private int stonesInside;
+    [SerializeField] private int answerCount;
 
-    private void ShowOracleMessage()
+    private void Start()
     {
-        int answer = Random.Range(0, OracleAnswers.Count);
-        OracleText.text = OracleAnswers[answer];
+        answerCount = DialogManager.Instance.Dialogues.First(dialog => dialog.characterName.Equals("SevenStar")).quests.Count;
+    }
+
+    private async void ShowOracleMessage()
+    {
+        var granny = GameManager.Instance.GrannyNormal;
+        GameManager.Instance.NearestVillager = granny;
+        int answer = Random.Range(0, answerCount);
+        granny.GetComponent<PlayMakerFSM>().FsmVariables.GetFsmInt("diceAnswer").Value = answer;
+        PlayMakerFSM.BroadcastEvent("DiceLevel");
+
+        await Task.Delay(2000);
+
+        foreach (Transform dice in DiceParent)
+        {
+            dice.GetComponent<ItemRespawner>().ReturnToOriginalPos();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
